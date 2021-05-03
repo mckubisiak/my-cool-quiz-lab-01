1068c6f1e081c5d57c8cf2a8ea5502b31bb9b445
Kubi Kubisiak <mckubisiak@gmail.com>
2021-05-03 16:56:27 -0700

    created intial layout in html

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 // import functions and grab DOM elements
-
 // initialize state
-
+// set event listeners to update state and DOMimport { checkForYes } from './utils.js';
+// import functions and grab DOM elements
+// initialize state
 // set event listeners to update state and DOM
+    // cool zone -- place where stuff happens when we click
+    // - launch an alert to tell the user they are about to take a quiz
+    // - launch a confirm
+    //     - if the user says no, break out of the quiz
+        // the word return STOPS any function. nothing gets past a return
+    // - get the user's first name
+        // - get the user's last name
+    // - launch a prompt with a question
+    //     - when the users answer question store the answer in variable
+    // the problem we have to solve is that users are free to type whatever they want, but we only want a yes or no. We can write a function that reads a user's input and tells us whether it's a yes or a no
+    // - evaluate the answer -- compare it to the correct answer
+    // if the user said yes, given them a point
+        // score++ also works
+        // score++ also works
+        // score++ also works
+    // - finally, display some "results" output in the DOM with the user's name and their score.
